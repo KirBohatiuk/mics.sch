78812b214d784852e831599ac8551f3b4cb2a7d1
kir <kirillbogatuk2005@gmail.com>
2024-06-27 07:48:00 +0300

    Done

--- MicsApp/urls.py
+++ MicsApp/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('contact-info', views.contact_info, name='contact-info'),
     path('assign-lesson', views.assign_lesson, name='assign-lesson'),
     path('blog', views.blog, name='blog'),
 ]
